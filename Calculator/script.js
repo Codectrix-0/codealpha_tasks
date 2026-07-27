3dafd871cad797e40a40df9eff7d0c27cba8fd5a
Divya Mer <divyamer066@gmail.com>
2026-07-27 20:48:32 +0530

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,1 +1,47 @@
+const display = document.getElementById("display");
 
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+
+    // Add click event to each button
+
+    button.addEventListener("click", function () {
+
+        let value = this.innerText;
+
+        // Clear display
+
+        if (value === "C") {
+            display.value = "";
+        }
+
+        // Remove last character
+
+        else if (value === "⌫") {
+            display.value = display.value.slice(0, -1);
+        }
+
+        // Calculate result
+
+        else if (value === "=") {
+
+            try {
+                display.value = eval(display.value);
+            }
+
+            catch {
+                display.value = "Error";
+            }
+
+        }
+
+        // Add clicked value
+
+        else {
+            display.value += value;
+        }
+
+    });
+
+});
